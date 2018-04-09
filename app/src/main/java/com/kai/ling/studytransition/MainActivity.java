26f3f38d14f7acd7362308650d349aad3eea7cad
kai.wang <kai.wang@aachuxing.com>
2018-04-09 17:47:39 +0800

    add AutoTransition

--- app/src/main/java/com/kai/ling/studytransition/MainActivity.java
+++ app/src/main/java/com/kai/ling/studytransition/MainActivity.java
@@ -1,13 +1,27 @@
 package com.kai.ling.studytransition;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.kai.ling.studytransition.systemtransition.TransitionActivity;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.btn_transitionactivity).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn_transitionactivity:
+                startActivity(new Intent(this, TransitionActivity.class));
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/kai/ling/studytransition/systemtransition/TransitionActivity.java
@@ -0,0 +1,53 @@
+package com.kai.ling.studytransition.systemtransition;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.transition.AutoTransition;
+import android.transition.TransitionManager;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.kai.ling.studytransition.R;
+
+public class TransitionActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private LinearLayout mRoot;
+    private TextView mTvAutoTransition;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_transition);
+
+        initView();
+    }
+
+    private void initView() {
+        mRoot = findViewById(R.id.root_view);
+        mTvAutoTransition = findViewById(R.id.tv_autotransition);
+
+        findViewById(R.id.btn_autotransition).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn_autotransition:
+                performAutoTransition();
+                break;
+        }
+    }
+
+    /**
+     * AutoTransition
+     */
+    private void performAutoTransition(){
+        TransitionManager.beginDelayedTransition(mRoot, new AutoTransition());
+        if (mTvAutoTransition.getVisibility() != View.VISIBLE) {
+            mTvAutoTransition.setVisibility(View.VISIBLE);
+        } else {
+            mTvAutoTransition.setVisibility(View.GONE);
+        }
+    }
+}
